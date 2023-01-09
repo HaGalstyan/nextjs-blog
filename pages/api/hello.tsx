ef4365f61eb849d7d8824cabb5fce0eb56646d51
HaGalstyan <hgalstyan@krisp.ai>
2023-01-09 22:58:23 +0400

    adding end

--- pages/api/hello.tsx
+++ pages/api/hello.tsx
@@ -3,4 +3,5 @@ import { NextApiRequest, NextApiResponse } from "next";
 // req = HTTP incoming message, res = HTTP server response
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   res.status(200).json({ text: "Hello" });
+  res.end();
 }
